ae909230f6d2dd61f9fba87b263d805ab74733a2
rutvik.d@gurukul.org <rutvik.d@gurukul.org>
2024-05-11 22:08:05 +0530

    some design change

--- src/app/grid/grid.module.ts
+++ src/app/grid/grid.module.ts
@@ -7,6 +7,8 @@ import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { FormsModule } from '@angular/forms';
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
-    FormsModule
+    FormsModule,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class GridModule { }
